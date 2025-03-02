dd011ab6dd7ec25be1754f7959552714ba0dcde4
TaylonSopeletto <gtataylon@gmail.com>
2025-03-02 17:37:00 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,102 +13,53 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-[90%] max-w-[1200px] m-auto">
+      <div className="mt-10 flex justify-between">
+        <div className="sm:flex gap-10">
+          <img className="h-auto sm:h-60 w-full sm:w-auto object-cover rounded-xl" src="https://avatars.githubusercontent.com/u/42319708?v=4"/>
+          <div className="flex flex-col gap-5 sm:h-full justify-center">
+            <h1 className="mt-6 sm:mt-0 text-xl font-bold">Taylon Sopeletto</h1>
+            <p className="text-lg">Full stack developer with a focus on front-end</p>
+            <div className="flex flex-wrap gap-5">
+              <a className="hover:underline hover:text-gray-100" href="http://linkedin.com/in/taylon-sopeletto-a1916317a"><i className="fa-brands fa-linkedin mr-3"></i>LinkedIn</a>
+              <a className="hover:underline hover:text-gray-100" href="http://github.com/TaylonSopeletto"><i className="fa-brands fa-github mr-3"></i>GitHub</a>
+              <a className="hover:underline hover:text-gray-100" href="mailto:gtataylon@gmail.com"><i className="fa-brands fa-google mr-3"></i>gtataylon@gmail.com</a>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <h2 className="mt-15 text-xl font-bold">Experience</h2>
+      <p className="text-md font-bold mt-5">Bucksense<span className="pl-2 font-normal text-sm">( 2022 - moment ) </span></p>
+      <p className="mb-5 text-sm">New York - United States</p>
+      <ul className="list-disc list-inside">
+        <li>Maintanance of e-commerce websites</li>
+        <li>Creating server side pages using NextJS</li>
+        <li>Working with Algolia to create a detailed search system</li>
+        <li>Working with Sanity as a CMS</li>
+        <li>Development of UI using React, NodeJS and Tailwind</li>
+      </ul>
+      <p className="text-md font-bold mt-5">NeoApp<span className="pl-2 font-normal text-sm">( 2020 - 2022 ) </span></p>
+      <p className="mb-5 text-sm">Cachoeiro de Itapemirim - Brazil</p>
+      <ul className="list-disc list-inside">
+        <li>Maintanance of e-commerce websites</li>
+        <li>Training AI models using TensorFlow</li>
+        <li>Development of UI using React, NodeJS and Tailwind</li>
+      </ul>
+
+      <h2 className="mt-15 text-xl font-bold">Languages</h2>
+      <p className="text-md font-bold mt-5">English</p>
+      <p className="mb-5 text-sm">Advanced</p>
+
+      <p className="text-md font-bold mt-5">Portuguese</p>
+      <p className="mb-5 text-sm">Fluent / Native</p>
+
+      <h2 className="mt-15 text-xl font-bold">Education</h2>
+      <p className="text-md font-bold mt-5">Data Science<span className="pl-2 font-normal text-sm">( 2024 - moment ) </span></p>
+      <p className="mb-5 text-sm">Descomplica</p>
+
+      <p className="text-md font-bold mt-5">Computer Science<span className="pl-2 font-normal text-sm">( incomplete ) </span></p>
+      <p className="mb-5 text-sm">Instituto Federal do Espirito Santo</p>
+     
     </div>
   );
 }
